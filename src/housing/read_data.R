dddedf73aedfc715b92af414789cf2eb0dabebd0
Josh Goldstein <jrg3bs@virginia.edu>
2022-06-17 09:46:26 -0400

    Read in BlackKnight housing data

--- src/housing/read_data.R
+++ src/housing/read_data.R
@@ -1,7 +1,12 @@
 # read in housing data
 
 library(sf)
+library(data.table)
 
 setwd("~/git/dspg22_coastal")
 
-path <- "~/../../../../project/biocomplexity/sdad/projects_data/coastal_futures/dspg2022"
+path <- "~/../../../../project/biocomplexity/sdad/projects_data/coastal_futures/dspg2022/"
+
+BKaccomack <- fread(paste0(path,"BlackKnight/accomack_BK_asmt_data.csv"))
+
+BKnorthampton <- fread(paste0(path,"BlackKnight/northampton_BK_asmt_data.csv"))
